dd246f410f4ae96147be69168be449780a440979
Emroze <emrozesd@users.noreply.github.com>
2019-04-27 12:49:35 -0700

    got game logic working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Game from "./components/Game";
 import './App.css';
 
-function App() {
-  return (
-    <Game></Game>
-  );
+
+class App extends Component {
+
+    state = {
+    gameId: 1,
+  };
+
+  resetGame = () =>
+    this.setState(prevState => ({
+      gameId: prevState.gameId + 1,
+    }));
+
+  render() {
+      return (
+        <Game
+          key={this.state.gameId}
+          autoPlay={this.state.gameId > 1}
+          challengeRange={[2, 9]}
+          challengeSize={6}
+          answerSize={4}
+          initialSeconds={15}
+          onPlayAgain={this.resetGame}
+        />
+      );
+  }
+
 }
 
 export default App;
--- client/src/components/Game/index.js
+++ client/src/components/Game/index.js
@@ -1,76 +1,136 @@
-import React from "react";
-import "./index.css"
+import React, { Component } from "react";
+import Number from "../Number/index.js";
+import _ from 'lodash';
+import "./index.css";
 
-function Game () {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-sm-3"></div>
-                <div className="col-sm-6">
-                    <div className="card">
-                        <div className="card-body text-center">
-                            <h1>42</h1>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-3"></div>
-            </div>
-            <div className="row">
-                <div className="col-sm-3"></div>
-                <div className="col-sm-3">
-                    <div className="card">
-                        <div className="card-body text-center">
-                            <h1>6</h1>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-3">
-                    <div className="card">
-                        <div className="card-body text-center">
-                            <h1>8</h1>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-3"></div>
-            </div>
-            <div className="row">
-                <div className="col-sm-3"></div>
-                <div className="col-sm-3">
-                    <div className="card">
-                        <div className="card-body text-center">
-                            <h1>3</h1>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-3">
-                    <div className="card">
-                        <div className="card-body text-center">
-                            <h1>2</h1>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-3"></div>
-            </div>
-            <div className="row">
-                <div className="col-sm-3"></div>
-                <div className="col-sm-3">
-                    <div className="card">
-                        <div className="card-body text-center">
-                            <h1>5</h1>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-3">
-                    <div className="card">
-                        <div className="card-body text-center">
-                            <h1>1</h1>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-3"></div>
-            </div>
-        </div>
+
+const randomNumberBetween = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const colors = {
+  new: 'lightblue',
+  playing: 'deepskyblue',
+  won: 'lightgreen',
+  lost: 'lightcoral',
+};
+
+
+class Game extends Component {
+  state = {
+    gameStatus: 'new', // new, playing, won, lost
+    remainingSeconds: this.props.initialSeconds,
+    selectedIds: [],
+  };
+  challengeNumbers = Array.from({
+    length: this.props.challengeSize,
+  }).map(() =>
+    randomNumberBetween(...this.props.challengeRange)
+  );
+  target = _.sum(
+    _.sampleSize(this.challengeNumbers, this.props.answerSize)
+  );
+  componentDidMount() {
+    if (this.props.autoPlay) {
+      this.startGame();
+    }
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  isNumberAvailable = numberIndex =>
+    this.state.selectedIds.indexOf(numberIndex) === -1;
+
+  startGame = () => {
+    this.setState({ gameStatus: 'playing' }, () => {
+      this.intervalId = setInterval(() => {
+        this.setState(prevState => {
+          const newRemainingSeconds =
+            prevState.remainingSeconds - 1;
+          if (newRemainingSeconds === 0) {
+            clearInterval(this.intervalId);
+            return { gameStatus: 'lost', remainingSeconds: 0 };
+          }
+          return { remainingSeconds: newRemainingSeconds };
+        });
+      }, 1000);
+    });
+  };
+
+  selectNumber = numberIndex => {
+    this.setState(
+      prevState => {
+        if (prevState.gameStatus !== 'playing') {
+          return null;
+        }
+        const newSelectedIds = 
+          [ ...prevState.selectedIds, numberIndex ];
+        return {
+          selectedIds: newSelectedIds,
+          gameStatus: this.calcGameStatus(newSelectedIds),
+        };
+      },
+      () => {
+        if (this.state.gameStatus !== 'playing') {
+          clearInterval(this.intervalId);
+        }
+      }
     );
+  };
+  calcGameStatus = newSelectedIds => {
+    const sumSelected = newSelectedIds.reduce(
+      (acc, curr) => acc + this.challengeNumbers[curr],
+      0
+    );
+    if (newSelectedIds.length !== this.props.answerSize) {
+      return 'playing';
+    }
+    return sumSelected === this.target ? 'won' : 'lost';
+  };
+
+  render() {
+    const { gameStatus, remainingSeconds } = this.state;
+    return (
+      <div className="game">
+        <div className="help">
+          Pick {this.props.answerSize} numbers that sum to the
+          target in {this.props.initialSeconds} seconds
+        </div>
+        <div
+          className="target"
+          style={{ backgroundColor: colors[gameStatus] }}
+        >
+          {gameStatus === 'new' ? 'TARGET' : this.target}
+        </div>
+        <div className="challenge-numbers">
+          {this.challengeNumbers.map((value, index) =>
+            <Number
+              key={index}
+              id={index}
+              value={gameStatus === 'new' ? '?' : value}
+              clickable={this.isNumberAvailable(index)}
+              onClick={this.selectNumber}
+            />
+          )}
+        </div>
+        <div className="footer">
+          {gameStatus === 'new' &&
+            <button onClick={this.startGame}>Start</button>
+          }
+
+          {gameStatus === 'playing' &&
+            <div className="timer-value">{remainingSeconds}</div>
+          }
+
+          {['won', 'lost'].includes(gameStatus) &&
+            <button onClick={this.props.onPlayAgain}>
+              Play Again
+            </button>
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Game
+export default Game;
