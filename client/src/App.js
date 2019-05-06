14dd927e977fef2a9497adac7abf94243ea831cd
Emroze <emrozesd@users.noreply.github.com>
2019-05-05 19:38:39 -0700

    added card animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import NavBar from './components/NavBar';
-import GameCarousel from "./components/GameCarousel";
-import GameMenu from "./components/GameMenu";
-import LeaderBoardNavs from "./components/LeaderBoardNavs";
-import OverallLeaderBoard from "./components/OverallLeaderBoard";
-import MemoryLeaderBoard from "./components/MemoryLeaderBoard";
-import MathLeaderBoard from "./components/MathLeaderBoard";
-import MinesLeaderBoard from "./components/MinesLeaderBoard";
-import TriviaLeaderBoard from "./components/TriviaLeaderBoard"
-import { Container, Row, Col } from "./components/Grid";
-import './App.css';
+import Home from "./pages/Home";
+import MathGame from "./pages/MathGame"
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <NavBar/>
-      <GameCarousel/>
-      <GameMenu/>
-      <LeaderBoardNavs/>
-      <OverallLeaderBoard/>
-      <MemoryLeaderBoard/>
-      <MathLeaderBoard/>
-      <MinesLeaderBoard/>
-      <TriviaLeaderBoard/>
-    </div>
+    <Router>
+      <div>
+        <Route path="/" component={Home}/>
+        <Route path="/MathGame" component={MathGame}/>
+      </div>
+    </Router>
     
   );
 }
--- client/src/components/GameMenu/index.js
+++ client/src/components/GameMenu/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
 import { Link } from 'react-router-dom'
 import { Container, Row, Col } from "../Grid";
 import MathIcon from "../HomePageImages/addition-icon.png";
@@ -7,44 +8,76 @@ import TriviaIcon from "../HomePageImages/trivia-icon.png";
 import MinesIcon from "../HomePageImages/minesweeper-icon.png";
 import Card from "react-bootstrap/Card";
 
-function GameMenu() {
-
+class GameMenu extends React.Component {
+  render() {
   return (
-        <Container>
+        <Container className="game-menu-container">
             <Row>
                 <Col size="sm-12">
-                    <Card>
-                        <Row>
-                            <Col size="sm-3">
-                                <Card>
-                                    <h4 className="text-center">Memory Lane</h4>
-                                <Card.Img src={MemoryIcon} alt="memory"/>
-                                </Card>
-                            </Col>
-                            <Col size="sm-3">
-                                <Card>
+                    <Row>
+                        <Col size="sm-3">
+                            <Flippy
+                                flipOnHover={true}
+                                flipDirection="horizontal"
+                            >
+                                <FrontSide>
+                                        <h4 className="text-center">Memory Lane</h4>
+                                    <Card.Img src={MemoryIcon} alt="memory"/>
+                                </FrontSide>
+                                <BackSide>
+                                    Game Screen Shot Here
+                                </BackSide>
+                            </Flippy>
+                        </Col>
+                        <Col size="sm-3">
+                             <Flippy
+                                flipOnHover={true}
+                                flipDirection="horizontal"
+                            >
+                                <FrontSide>
                                     <h4 className="text-center">Math Sprint</h4>
                                     <Link to="/MathGame"><Card.Img src={MathIcon} alt="math"/></Link>
-                                </Card>
-                            </Col>
-                            <Col size="sm-3">
-                                <Card>
-                                <h4 className="text-center">Field Navigator</h4>
-                                <Card.Img src={MinesIcon} alt="mines"/>
-                                </Card>
-                            </Col>
-                            <Col size="sm-3">
-                                <Card>
-                                <h4 className="text-center">Quiz Bowl</h4>
+                                </FrontSide>
+                                <BackSide>
+                                    Game Screen Shot Here
+                                </BackSide>
+                            </Flippy>
+                        </Col>
+                        <Col size="sm-3">
+                             <Flippy
+                                flipOnHover={true}
+                                flipDirection="horizontal"
+                            >
+                                <FrontSide>
+                                    <h4 className="text-center">Field Navigator</h4>
+                                    <Card.Img src={MinesIcon} alt="mines"/>
+                                </FrontSide>
+                                <BackSide>
+                                    Game Screen Shot Here
+                                </BackSide>
+                            </Flippy>
+                        </Col>
+                        <Col size="sm-3">
+                            <Flippy
+                                flipOnHover={true}
+                                flipDirection="horizontal"
+                            >
+                                <FrontSide>
+                                    <h4 className="text-center">Quiz Bowl</h4>
                                     <Card.Img src={TriviaIcon} alt="trivia"/>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Card>
+                                </FrontSide>
+                                <BackSide>
+                                    Game Screen Shot Here
+                                </BackSide>
+                            </Flippy>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
           </Container>
     );
+
+  }
 
 }
 
