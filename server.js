aa707fa2135309b66fa542a4b746f7ec1c108778
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-05-09 19:33:45 -0700

    added server js file containing server for db

---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/apiRoutes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+var db = require("./models");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Sets requests to use json and set req.body
+
+app.use('*', (req, res, next) => {
+  console.log(req);
+  next();
+})
+
+// Add routes, both API and view
+app.use(routes);
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
