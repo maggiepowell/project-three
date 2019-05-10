594c13f2aad700213ca76201aea10b3d26d9af2a
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-05-09 21:02:55 -0700

    added import for overall and memory leader boards

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,12 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import NavTabs from "./NavTabs";
 import Home from "./pages/Home";
 import MathGame from "./pages/MathGame";
-import MemoryGame from "./pages/MemoryGame";
-import FieldNavGame from "./pages/FieldNavGame";
-import Trivia from "./pages/Trivia";
+// import MemoryGame from "./pages/MemoryGame";
+// import FieldNavGame from "./pages/FieldNavGame";
+// import Trivia from "./pages/Trivia";
+import OverallLeaderBoard from "./components/OverallLeaderBoard";
+import MemoryLeaderBoard from "./components/MemoryLeaderBoard";
+
 
 function App() {
   return (
@@ -14,9 +17,9 @@ function App() {
       <NavTabs />
       <Route exact path="/" component={Home}/>
         <Route exact path="/MathGame" component={MathGame}/>
-        <Route exact path="/MemoryGame" component={MemoryGame}/>
+        {/* <Route exact path="/MemoryGame" component={MemoryGame}/>
         <Route exact path="/FieldNavGame" component={FieldNavGame}/>
-        <Route exact path="/Trivia" component={Trivia}/>
+        <Route exact path="/Trivia" component={Trivia}/> */}
     </Router>
     </div>
   );
