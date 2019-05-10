8ee1c21aeb5203f7ebcd0207e7648342d942b5ce
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-05-09 21:16:55 -0700

    commented out routes not in place

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,11 @@ import MathGame from "./pages/MathGame";
 // import MemoryGame from "./pages/MemoryGame";
 // import FieldNavGame from "./pages/FieldNavGame";
 // import Trivia from "./pages/Trivia";
-import OverallLeaderBoard from "./components/OverallLeaderBoard";
-import MemoryLeaderBoard from "./components/MemoryLeaderBoard";
-import MathLeaderBoard from "./components/MathLeaderBoard";
-import MinesLeaderBoard from "./components/MinesLeaderBoard";
-import TriviaLeaderBoard from "./components/TriviaLeaderBoard";
+// import OverallLeaderBoard from "./components/OverallLeaderBoard";
+// import MemoryLeaderBoard from "./components/MemoryLeaderBoard";
+// import MathLeaderBoard from "./components/MathLeaderBoard";
+// import MinesLeaderBoard from "./components/MinesLeaderBoard";
+// import TriviaLeaderBoard from "./components/TriviaLeaderBoard";
 
 function App() {
   return (
@@ -22,11 +22,11 @@ function App() {
         {/* <Route exact path="/MemoryGame" component={MemoryGame}/>
         <Route exact path="/FieldNavGame" component={FieldNavGame}/>
         <Route exact path="/Trivia" component={Trivia}/> */}
-        <Route exact path="/OverallLeaderBoard" component={OverallLeaderBoard}/>
+        {/* <Route exact path="/OverallLeaderBoard" component={OverallLeaderBoard}/>
         <Route exact path="/MemoryLeaderBoard" component={MemoryLeaderBoard}/>
         <Route exact path="/MathLeaderBoard" component={MathLeaderBoard}/>
         <Route exact path="/MinesLeaderBoard" component={MinesLeaderBoard}/>
-        <Route exact path="/TriviaLeaderBoard" component={TriviaLeaderBoard}/>
+        <Route exact path="/TriviaLeaderBoard" component={TriviaLeaderBoard}/> */}
     </Router>
     </div>
   );
