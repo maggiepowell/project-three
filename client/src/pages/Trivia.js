c4428b816eddd198a7798a1b1fbd0eee885a64ee
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-05-11 13:36:17 -0700

    added renderQuiz and renderResult functions

--- client/src/pages/Trivia.js
+++ client/src/pages/Trivia.js
@@ -3,6 +3,26 @@ import Quiz from "../components/trivia";
 import Results from "../components/trivia";
 
 class Trivia extends Component {
+
+  renderQuiz() {
+    return (
+      <Quiz
+        answer={this.state.answer}
+        answerOptions={this.state.answerOptions}
+        questionId={this.state.questionId}
+        question={this.state.question}
+        questionTotal={quizQuestions.length}
+        onAnswerSelected={this.handleAnswerSelected}
+      />
+    );
+  }
+  
+  renderResult() {
+    return (
+      <Result quizResult={this.state.result} />
+    );
+  }
+
   render() {
       return (
         {this.state.result ? this.renderResult() : this.renderQuiz()}
