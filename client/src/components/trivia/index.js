3adbe3a5bc94209b4570da1ac8a7d6214c2fadaa
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-05-11 11:46:45 -0700

    added function setNextQuestion

--- client/src/components/trivia/index.js
+++ client/src/components/trivia/index.js
@@ -89,6 +89,18 @@ class Quiz extends React.Component {
       }
   }
 
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+    this.setState({
+      counter: counter,
+      questionId: questionId,
+      question: quizQuestions[counter].question,
+      answerOptions: quizQuestions[counter].answers,
+      answer: ''
+    });
+  }
+
   render() {
     return (
         <div className="quiz">
