950cd048d631cb0f620f0b76f47808ae7a505cee
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-05-14 20:13:02 -0700

    renamed Index to TriviaApp

---
+++ client/src/components/trivia/TriviaApp.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Question from './Question';
+import Quiz from './Quiz';
+import QuestionCount from './QuestionCount';
+import AnswerOption from './AnswerOption';
+import quizQuestions from './api/quizQuestions';
+import Result from './trivia';
+
+class Index extends React.Component {
+
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      counter: 0,
+      questionId: 1,
+      question: '',
+      answerOptions: [],
+      answer: '',
+      answersCount: {
+        nintendo: 0,
+        microsoft: 0,
+        sony: 0
+      },
+      result: ''
+    };
+
+    this.renderAnswerOptions = this.renderAnswerOptions.bind(this);
+  }
+
+//The componentWillMount life cycle event is invoked once, both on the client and server, immediately before the initial rendering occurs.
+//When you call setState within this method as we are above on line 4, render() will see the updated state and it will be executed only once despite the state change
+  componentWillMount() {
+    const shuffledAnswerOptions = quizQuestions.map((question) => this.shuffleArray(question.answers));  
+  
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions: shuffledAnswerOptions[0]
+    });
+  }  
+
+  //randomise the order of the answer options
+  shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  };  
+  renderAnswerOptions(key) {
+    return (
+      <AnswerOption
+        key={key.content}
+        answerContent={key.content}
+        answerType={key.type}
+        answer={this.props.answer}
+        questionId={this.props.questionId}
+        onAnswerSelected={this.props.onAnswerSelected}
+      />
+    );
+  }
+
+  setUserAnswer(answer) {
+    this.setState((state) => ({
+      answersCount: {
+        ...state.answersCount,
+        [answer]: state.answersCount[answer] + 1
+      },
+      answer: answer
+    }));
+  }
+
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+    this.setState({
+      counter: counter,
+      questionId: questionId,
+      question: quizQuestions[counter].question,
+      answerOptions: quizQuestions[counter].answers,
+      answer: ''
+    });
+  }
+
+  handleAnswerSelected(event) {
+    this.setUserAnswer(event.currentTarget.value);
+    if (this.state.questionId < quizQuestions.length) {
+        setTimeout(() => this.setNextQuestion(), 300);
+      } else {
+        setTimeout(() => this.setResults(this.getResults()), 300);
+            }
+  }
+
+  getResults() {
+    const answersCount = this.state.answersCount;
+    const answersCountKeys = Object.keys(answersCount);
+    const answersCountValues = answersCountKeys.map((key) => answersCount[key]);
+    const maxAnswerCount = Math.max.apply(null, answersCountValues);
+  
+    return answersCountKeys.filter((key) => answersCount[key] === maxAnswerCount);
+  }
+
+  setResults (result) {
+    if (result.length === 1) {
+      this.setState({ result: result[0] });
+    } else {
+      this.setState({ result: 'Undetermined' });
+    }
+  }
+
+  renderQuiz() {
+    return (
+      <Quiz
+        answer={this.state.answer}
+        answerOptions={this.state.answerOptions}
+        questionId={this.state.questionId}
+        question={this.state.question}
+        questionTotal={quizQuestions.length}
+        onAnswerSelected={this.handleAnswerSelected}
+      />
+    );
+  }
+
+  renderResult() {
+    return (
+      <Result quizResult={this.state.result} />
+    );
+  }
+
+  render() {
+    return (
+        <div className="quiz">
+          <QuestionCount
+            counter={this.state.questionId}
+            total={this.state.questionTotal}
+          />
+          <Question content={this.state.question} />
+          <ul className="answerOptions">
+            {this.state.answerOptions.map(this.renderAnswerOptions)}
+          </ul>
+        </div>
+    );
+  }
+}
+  
+  Quiz.propTypes = {
+    answer: PropTypes.string.isRequired,
+    answerOptions: PropTypes.array.isRequired,
+    counter: PropTypes.number.isRequired,
+    question: PropTypes.string.isRequired,
+    questionId: PropTypes.number.isRequired,
+    questionTotal: PropTypes.number.isRequired,
+    onAnswerSelected: PropTypes.func.isRequired
+  };
+  
+  export default Index;
+  
