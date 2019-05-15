916e74fe6ccb2df13a7d0557faf67407f9e8b5b3
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-05-14 20:15:16 -0700

    changed Index to TriviaApp on line 9

--- client/src/pages/Trivia.js
+++ client/src/pages/Trivia.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Index from "../components/trivia";
+import TriviaApp from "../components/trivia/TriviaApp";
 
 class Trivia extends Component {
 
 
   render() {
       return (
-        <Index />
+        <TriviaApp />
       );
   }
 
