16d348cb3ea3db2c7c4a8398d565571e931a20a7
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-05-16 20:18:33 -0700

    updated results message

--- client/src/components/trivia/Results.js
+++ client/src/components/trivia/Results.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 class Result extends React.Component {
-  constructor(props){
-    super(props);
-  }
-  renderQuestins(){
-    return  this.props.quizResult.map((_data,index) =>{
-        return <div className="list-grp">{_data.question} <br/>correct answer is option: {_data.answerindex} :  and you have selcted {this.props.answers[index]+1} { ((this.props.answers[index]+1) === _data.answerindex) ? (<span className="status">its correct !man</span>) : ''}</div>
-    })
-  }
+  // constructor(props){
+  //   super(props);
+  // }
+  // renderQuestins(){
+  //   return  this.props.quizResult.map((_data,index) =>{
+  //       return <div className="list-grp">{_data.question} <br/>correct answer is option: {_data.answerindex} :  and you have selcted {this.props.answers[index]+1} { ((this.props.answers[index]+1) === _data.answerindex) ? (<span className="status">its correct !man</span>) : ''}</div>
+  //   })
+  // }
   render (){
     return (
     <div  className="quiz-story">
       <div>
         <strong>Lets see your results</strong>!
-        <div>{this.renderQuestins()}</div>
+        <div>Great Job! Keep up the hard work and you can achieve your dreams!</div>
       </div>
     </div>
     )
